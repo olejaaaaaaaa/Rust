432e3297414b5cedc5d6d1f6082bcc6c0241208f
Oleja <162372805+olejaaaaaaaa@users.noreply.github.com>
2024-06-17 19:27:40 +0300

    Update main.rs

--- banana/src/main.rs
+++ banana/src/main.rs
@@ -9,95 +9,19 @@ use std::f64::consts::PI;
 use std::time::Instant;
 
 
-fn collision_line_vs_line(p0: ((f64, f64), (f64, f64)), p1: ((f64, f64), (f64, f64)) ) -> (f64, f64) {
 
-    let x1 = p0.0.0;
-    let y1 = p0.0.1;
-
-    let x2 = p0.1.0;
-    let y2 = p0.1.1;
-
-    let K1 = ((y2-y1)-x1*(y2-y1))/(x2-x1);
-    let B1 = y1;
-    println!("{:?}*x + {:?}", K1, B1);
-
-    let x1 = p1.0.0;
-    let y1 = p1.0.1;
-
-    let x2 = p1.1.0;
-    let y2 = p1.1.1;
-
-    let K2 = ((y2-y1)-x1*(y2-y1))/(x2-x1);
-    let B2 = y1;
-    println!("{:?}*x + {:?}", K2, B2);
-
-    let X = (B2 - B1)/(K1 - K2);
-    let Y = X*K2 + B2;
-
-    println!("y: {:?}", X*K2 + B2);
-    println!("y: {:?}", X*K1 + B1);
-
-
-    return (X, Y);
-}
-
-
-fn len(point0: (f64, f64), point1: (f64, f64)) -> f64 {
-    let x1 = point0.0;
-    let y1 = point0.1;
-
-    let x2 = point1.0;
-    let y2 = point1.1;
-
-    let res = (x2 - x1).powi(2) + (y2 - y1).powi(2);
-
-    return res.sqrt()
-}
-
-fn set_angle_C(angle: i32, point: (f64, f64), line: ((f64, f64), (f64, f64))) -> ((f64, f64), (f64, f64)) {
-
-    // сука, я сам тут хер что разберу
-    let r1 = len(point, line.0);
-    let tmp_point = (line.0.0, point.1);
-    let p1 = len(tmp_point, point);
-
-    let cos = p1/r1;
-
-    let ang = cos.acos() + (angle as f64 / 57.3);
-
-    
-
-    let point1_x = ang.cos() * r1;
-    let point1_y = ang.sin() * r1;
-
-
-    let r1 = len(point, line.1);
-    let tmp_point = (line.1.0, point.1);
-    let p1 = len(tmp_point, point);
-
-    let cos = p1/r1;
-
-    let ang = cos.acos() + (angle as f64 / 57.3);
-
-    
-
-    let point2_x = ang.cos() * r1;
-    let point2_y = ang.sin() * r1;
-
-
-    return ((point1_x, point1_y), (point2_x, point2_y));
-}
 
 
 fn main() {
-    // We start by creating the EventLoop, this can only be done once per process.
-    // This also needs to happen on the main thread to make the program portable.
+
+
+    
     let event_loop = winit::event_loop::EventLoopBuilder::new()
         .build()
         .expect("event loop building");
     let (_window, display) = glium::backend::glutin::SimpleWindowBuilder::new()
     .with_inner_size(640, 640)
-        .with_title("Glium tutorial #2")
+        .with_title("Avokado")
         .build(&event_loop);
     
 
